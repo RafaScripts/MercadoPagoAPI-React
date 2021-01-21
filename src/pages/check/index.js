1a18a375d7c61dc269f51b1464a592174606acd0
Rafael <rafaelfreire0@gmail.com>
2021-01-21 00:16:25 -0300

    init
    esqueleto do script

---
+++ src/pages/check/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+import {FiLogIn} from 'react-icons/fi'
+
+import { Link, useHistory } from "react-router-dom"
+
+import './styles.css';
+
+//import heroesImg from '../../assets/heroes.png';
+
+//import logoImg from '../../assets/logo.svg';
+
+import api from '../../services/api';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+export default function Logon() {
+    const [id, setId] = useState('');
+    const [key, setKey] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+     
+        try {
+          const response = await api.post('sessions', { id, key });
+
+          localStorage.setItem('ongId', id);
+          localStorage.setItem('ongName', response.data.name);
+
+          history.push('/profile');
+        } catch (err) {
+            alert('Falha no logon tente novamente.');
+        }
+    
+    }
+
+    return (
+        <div className="logon-conteiner">
+            <section className="form">
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu Logon</h1>
+                    <input
+                      placeholder="Sua ID"
+                      value={id}
+                      onChange={e => setId(e.target.value)}
+                    />
+
+                    <input
+                      placeholder="Sua Key"
+                      value={key}
+                      onChange={e => setKey(e.target.value)}
+                    />
+
+
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color= "#e02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            
+        </div>
+    );
+}
